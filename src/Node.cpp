291b10e14ef588e0198857010d60b855865b2baa
Mahmoud Zeidan <mahmoud.zeidan@mail.utoronto.ca>
2023-05-16 22:36:27 -0400

    Implemented Node::GetModelMatrix()

--- src/Node.cpp
+++ src/Node.cpp
@@ -1,5 +1,7 @@
 #include "Node.h"
 #include <iostream>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/quaternion.hpp>
 
 using namespace Atrium;
 
@@ -9,15 +11,24 @@ Node::Node(const tinygltf::Node& gltfNode) {
 
 	children = std::vector<Node*>();
 
-	rotation = gltfNode.rotation.size() != 0 
+	glm::vec4 rotation = gltfNode.rotation.size() != 0 
 		? glm::vec4(gltfNode.rotation[0], gltfNode.rotation[1], gltfNode.rotation[2], gltfNode.rotation[3])
-		: glm::vec4(0.0f);
+		: glm::vec4(0.0f, 0.0f, 0.0f, 1.0f);
+	quaternion = glm::fquat(rotation.w, rotation.x, rotation.y, rotation.z);
+
 	scale = gltfNode.scale.size() != 0 
 		? glm::vec3(gltfNode.scale[0], gltfNode.scale[1], gltfNode.scale[2]) 
 		: glm::vec3(1.0f);
 	translation = gltfNode.translation.size() != 0 
 		? glm::vec3(gltfNode.translation[0], gltfNode.translation[1], gltfNode.translation[2]) 
 		: glm::vec3(0.0f);
+}
+glm::mat4 Node::GetModelMatrix() {
+	glm::mat4 rotationMatrix = glm::toMat4(quaternion);
+	glm::mat4 scaleMatrix = glm::scale(glm::mat4(1.0f), scale);
+	glm::mat4 translationMatrix = glm::translate(glm::mat4(1.0f), translation);
+
+	return translationMatrix * scaleMatrix * rotationMatrix;
 }
 std::string Node::ToString(unsigned int depth) const{
 	std::string str = "-" + name;
--- src/Node.h
+++ src/Node.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <TinyGLTF/tiny_gltf.h>
 #include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
 
 namespace Atrium {
 	enum class NodeType {
@@ -14,12 +15,13 @@ namespace Atrium {
 	public:
 		Node(const tinygltf::Node& gltfNode);
 		std::string ToString(unsigned int depth = 0) const;
+		glm::mat4 GetModelMatrix();
 	public:
 		std::string name;
 		NodeType type;
 		std::vector<Node*> children;
 	private:
-		glm::vec4 rotation;
+		glm::fquat quaternion;
 		glm::vec3 scale;
 		glm::vec3 translation;
 	};
