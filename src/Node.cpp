00e46218c99c1b6986a57fe794dc9c33483433ae
Mahmoud Zeidan <mahmoud.zeidan@mail.utoronto.ca>
2023-05-16 22:21:29 -0400

    Node now loads transformation data

--- src/Node.cpp
+++ src/Node.cpp
@@ -6,7 +6,18 @@ using namespace Atrium;
 Node::Node(const tinygltf::Node& gltfNode) {
 	name = gltfNode.name;
 	type = NodeType::Node;
+
 	children = std::vector<Node*>();
+
+	rotation = gltfNode.rotation.size() != 0 
+		? glm::vec4(gltfNode.rotation[0], gltfNode.rotation[1], gltfNode.rotation[2], gltfNode.rotation[3])
+		: glm::vec4(0.0f);
+	scale = gltfNode.scale.size() != 0 
+		? glm::vec3(gltfNode.scale[0], gltfNode.scale[1], gltfNode.scale[2]) 
+		: glm::vec3(1.0f);
+	translation = gltfNode.translation.size() != 0 
+		? glm::vec3(gltfNode.translation[0], gltfNode.translation[1], gltfNode.translation[2]) 
+		: glm::vec3(0.0f);
 }
 std::string Node::ToString(unsigned int depth) const{
 	std::string str = "-" + name;
--- src/Node.h
+++ src/Node.h
@@ -3,14 +3,13 @@
 #include <string>
 #include <vector>
 #include <TinyGLTF/tiny_gltf.h>
-
+#include <glm/glm.hpp>
 
 namespace Atrium {
 	enum class NodeType {
 		Node,
 		Mesh
 	};
-
 	class Node {
 	public:
 		Node(const tinygltf::Node& gltfNode);
@@ -19,6 +18,10 @@ namespace Atrium {
 		std::string name;
 		NodeType type;
 		std::vector<Node*> children;
+	private:
+		glm::vec4 rotation;
+		glm::vec3 scale;
+		glm::vec3 translation;
 	};
 }
 
