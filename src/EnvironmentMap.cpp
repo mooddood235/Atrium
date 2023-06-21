35917e7b2896490a8f7e790d9b768a7487d7adce
Mahmoud Zeidan <mahmoud.zeidan@mail.utoronto.ca>
2023-06-21 16:36:53 -0400

    Implemented EnvrionmentMap::CreateBins()

--- src/EnvironmentMap.cpp
+++ src/EnvironmentMap.cpp
@@ -5,15 +5,17 @@
 using namespace Atrium;
 
 EnvironmentMap::EnvironmentMap(const std::string& path){
-	LoadEnvironmentMap(path);
+	float* data = LoadEnvironmentMap(path);
+	CreateBins((glm::vec3*)data, 0, width - 1, 0, height - 1);
+	stbi_image_free(data);
 }
-void EnvironmentMap::LoadEnvironmentMap(const std::string& path) {
+float* EnvironmentMap::LoadEnvironmentMap(const std::string& path) {
 	stbi_set_flip_vertically_on_load(true);
 
 	glGenTextures(1, &mapID);
 	glBindTexture(GL_TEXTURE_2D, mapID);
 
-	int width, height, numChannels;
+	int numChannels;
 	float* data = stbi_loadf(path.c_str(), &width, &height, &numChannels, 0);
 
 	if (!data) {
@@ -21,9 +23,7 @@ void EnvironmentMap::LoadEnvironmentMap(const std::string& path) {
 		stbi_image_free(data);
 		exit(-1);
 	}
-
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB32F, width, height, 0, GL_RGB, GL_FLOAT, data);
-	stbi_image_free(data);
 
 	glGenerateMipmap(GL_TEXTURE_2D);
 
@@ -33,6 +33,34 @@ void EnvironmentMap::LoadEnvironmentMap(const std::string& path) {
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
 	glBindTexture(GL_TEXTURE_2D, 0);
+	return data;
+}
+void EnvironmentMap::CreateBins(const glm::vec3* data, unsigned int x0, unsigned int x1, unsigned int y0, unsigned int y1) {
+	unsigned int w = x1 - x0;
+	unsigned int h = y1 - y0;
+
+	glm::vec3 radiance = glm::vec3(0.0f);
+
+	for (unsigned int x = x0; x <= x1; x++) {
+		for (unsigned int y = y0; y <= y1; y++) {
+			unsigned int flat = x + y * width;
+			radiance += data[flat];
+		}
+	}
+	if (glm::length(radiance) <= glm::length(minRadiance) || w * h <= smallestBinArea || w * h == 0) {
+		bins.push_back(glm::vec4(x0, x1, y0, y1));
+		return;
+	}
+	if (w > h) {
+		unsigned int xSplit = w / 2 + x0;
+		CreateBins(data, x0, xSplit, y0, y1);
+		CreateBins(data, xSplit + 1, x1, y0, y1);
+	}
+	else {
+		unsigned int ySplit = h / 2 + y0;
+		CreateBins(data, x0, x1, y0, ySplit);
+		CreateBins(data, x0, x1, ySplit + 1, y1);
+	}
 }
 unsigned int EnvironmentMap::GetMapID() const {
 	return mapID;
--- src/EnvironmentMap.h
+++ src/EnvironmentMap.h
@@ -1,7 +1,6 @@
 #pragma once
 #include "ShaderProgram.h"
 #include <glm/glm.hpp>
-#include <limits>
 
 namespace Atrium {
 	struct EnvironmentMap{
@@ -9,10 +8,14 @@ namespace Atrium {
 		EnvironmentMap(const std::string& path);
 		unsigned int GetMapID() const;
 	private:
-		void LoadEnvironmentMap(const std::string& path);
+		float* LoadEnvironmentMap(const std::string& path);
+		void CreateBins(const glm::vec3* data, unsigned int x0, unsigned int x1, unsigned int y0, unsigned int y1);
 	private:
 		unsigned int mapID;
+		int width, height;
 		std::vector<glm::vec4> bins;
+		const float minRadiance = 1.0f;
+		const float smallestBinArea = 1000.0f;
 	};
 }
 
