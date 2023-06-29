a787243f180e563af08e5e47dd58fdc22be09227
Mahmoud Zeidan <mahmoud.zeidan@mail.utoronto.ca>
2023-06-28 20:02:25 -0400

    Implemented GGX from PBRT

--- src/Shaders/BXDFHelpers.glsl
+++ src/Shaders/BXDFHelpers.glsl
@@ -1,7 +1,5 @@
 #include "Math.glsl"
 
-
-
 struct BXDFSample{
 	vec3 f;
 	float pdf;
@@ -30,6 +28,11 @@ float SampleHemispherePDF(){
 }
 float CosineSampleHemispherePDF(float cosTheta){
 	return cosTheta / PI;
+}
+vec2 SampleUniformDiskPolar(float u0, float u1) {
+    float r = SafeSqrt(u0);
+    float theta = 2.0 * PI * u1;
+    return normalize(vec2(r * cos(theta), r * sin(theta)));
 }
 bool Refract(vec3 wi, float eta, out float etap, out vec3 wt){
 	vec3 n = vec3(0.0, 0.0, 1.0);
@@ -89,3 +92,45 @@ vec3 F0ToIOR(vec3 F0){
 	F0 = min(vec3(0.9999), F0);
 	return (1.0 + F0) / (1.0 - F0);
 }
+float D_ggx(vec3 m, float a){
+	float tan2Theta = Tan2Theta(m);
+    if (isinf(tan2Theta)) return 0.0;
+    float cos4Theta = Sqr(Cos2Theta(m));
+    if (cos4Theta < 1e-16f) return 0.0;
+    float e = tan2Theta * (Sqr(CosPhi(m) / a) + Sqr(SinPhi(m) / a));
+    return 1.0 / (PI * a * a * cos4Theta * Sqr(1.0 + e));
+}
+float Lambda(vec3 w, float a) {
+    float tan2Theta = Tan2Theta(w);
+    if (isinf(tan2Theta)) return 0.0;
+    float alpha2 = Sqr(CosPhi(w) * a) + Sqr(SinPhi(w) * a);
+    return (sqrt(1 + alpha2 * tan2Theta) - 1.0) / 2.0;
+}
+float G1(vec3 w, float a) { return 1.0 / (1.0 + Lambda(w, a)); }
+float G(vec3 wo, vec3 wi, float a) { return 1.0 / (1.0 + Lambda(wo, a) + Lambda(wi, a)); }
+float VNDF_ggx(vec3 w, vec3 m, float a) {
+        return G1(w, a) / AbsCosTheta(w) * D_ggx(m, a) * AbsDot(w, m);
+}
+vec3 SampleVNDF_ggx(vec3 w, float a, float u0, float u1) {
+        // Transform _w_ to hemispherical configuration
+        vec3 wh = normalize(vec3(a * w.x, a * w.y, w.z));
+        if (wh.z < 0.0) wh = -wh;
+
+        // Find orthonormal basis for visible normal sampling
+        vec3 T1 = (wh.z < 0.99999f) ? normalize(cross(vec3(0.0, 0.0, 1.0), wh))
+                                        : vec3(1.0, 0.0, 0.0);
+        vec3 T2 = cross(wh, T1);
+
+        // Generate uniformly distributed points on the unit disk
+        vec2 p = SampleUniformDiskPolar(u0, u1);
+
+        // Warp hemispherical projection for visible normal sampling
+        float h = sqrt(1.0 - Sqr(p.x));
+        p.y = mix((1.0 + wh.z) / 2.0, h, p.y);
+
+        // Reproject to hemisphere and transform normal to ellipsoid configuration
+        float pz = sqrt(max(0.0, 1.0 - Sqr(length(p))));
+        vec3 nh = p.x * T1 + p.y * T2 + pz * wh;
+        //CHECK_RARE(1e-5f, nh.z == 0);
+        return normalize(vec3(a * nh.x, a * nh.y, max(1e-6f, nh.z)));
+    }
--- src/Shaders/ConductorBSDF.glsl
+++ src/Shaders/ConductorBSDF.glsl
@@ -1,4 +1,5 @@
 #include "BXDFHelpers.glsl"
+#include "Material.glsl"
 
 vec3 f_ConductorBSDF(vec3 wo, vec3 wi){
 	if (!SameHemisphere(wo, wi)) return vec3(0.0);
@@ -9,10 +10,40 @@ vec3 pdf_ConductorBSDF(vec3 wo, vec3 wi){
 	return vec3(0.0);
 }
 
-BXDFSample SampleConductorBSDF(vec3 wo, vec3 albedo){
-	vec3 wi = vec3(-wo.x, -wo.y, wo.z);
-	vec3 f = FSchlick(AbsCosTheta(wi), albedo) / AbsCosTheta(wi); 
-	float pdf = 1.0;
+BXDFSample SampleConductorBSDF(vec3 wo, Material mat, float u0, float u1){
+//	vec3 wi = vec3(-wo.x, -wo.y, wo.z);
+//	vec3 f = FSchlick(AbsCosTheta(wi), albedo) / AbsCosTheta(wi); 
+//	float pdf = 1.0;
+//
+//	return BXDFSample(f, pdf, wi);
+    
+    if (mat.roughness <= 0.001) {
+        // Sample perfect specular conductor BRDF
+        vec3 wi = vec3(-wo.x, -wo.y, wo.z);
+        vec3 f = FSchlick(AbsCosTheta(wi), mat.albedo) / AbsCosTheta(wi); 
+        float pdf = 1.0;
+        return BXDFSample(f, pdf, wi);
+    }
+    // Sample rough conductor BRDF
+    // Sample microfacet normal $\wm$ and reflected direction $\wi$
+    if (wo.z == 0.0)
+        return InvalidSample;
+    vec3 wm = SampleVNDF_ggx(wo, mat.roughness, u0, u1);
+    vec3 wi = reflect(-wo, wm);
+//    if (!SameHemisphere(wo, wi))
+//        return InvalidSample;
 
-	return BXDFSample(f, pdf, wi);
+    // Compute PDF of _wi_ for microfacet reflection
+    float pdf = VNDF_ggx(wo, wm, mat.roughness) / (4.0 * AbsDot(wo, wm));
+
+    float cosTheta_o = AbsCosTheta(wo), cosTheta_i = AbsCosTheta(wi);
+    if (cosTheta_i == 0.0 || cosTheta_o == 0.0)
+        return InvalidSample;
+    // Evaluate Fresnel factor _F_ for conductor BRDF
+    vec3 F = FSchlick(AbsCosTheta(wi), mat.albedo) / AbsCosTheta(wi); 
+
+    vec3 f =
+        F / max(EPSILON, 4.0 * cosTheta_i * cosTheta_o);
+    f = F;
+    return BXDFSample(f, pdf, wi);
 }
