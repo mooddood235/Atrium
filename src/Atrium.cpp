7bdb16555cee251d3b177417933eed5024536430
Mahmoud Zeidan <mahmoud.zeidan@mail.utoronto.ca>
2023-07-07 15:10:36 -0400

    Removed A bunch of scenes

--- src/Atrium.cpp
+++ src/Atrium.cpp
@@ -25,7 +25,7 @@ int main()
     Atrium::RenderCamera::Init();
     
     // Objects
-    Atrium::Scene scene = Atrium::Scene("Models/SprayPaint/SprayPaint.gltf");
+    Atrium::Scene scene = Atrium::Scene("Models/CornellBox.gltf");
     std::cout << scene.ToString() << std::endl;
 
     Atrium::Camera* camera = scene.cameras[0];
